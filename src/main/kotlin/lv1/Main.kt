d6bf282d0ca2ece0905d51bc9cc57c15d072fe6b
hongjinkwon <hjk0472@gmail.com>
2024-04-29 11:51:52 +0900

    🎨Feat: 출력 게임 시작 시 안내문구
    필수 구현단계의 모든 코드 작성 진행 후,
    시작 시 선택 옵션 적용 예정 (게임시작, 기록보기, 게임종료)

--- src/main/kotlin/lv1/Main.kt
+++ src/main/kotlin/lv1/Main.kt
@@ -1,12 +1,11 @@
 package org.example.lv1
 
 fun main() {
-    val name = "Kotlin"
 
-    println("Hello, " + name + "!")
+    println("환영합니다! 숫자 야구게임을 시작합니다.")
+    println("Rule1 : 숫자 3자리를 입력해주세요.")
+    println("Rule2 : 1에서 9 사이 숫자를 입력해야하며 동일한 숫자는 입력할 수 없습니다.")
+    print("게임을 시작합니다! 원하시는 숫자 3자리를 입력해주세요 : ")
 
-    for (i in 1..5) {
-
-        println("i = $i")
-    }
+    val input: Int = readln().toInt()
 }
