1b242cc392b8024c51e34ad4f1275a3b11dbd020
hongjinkwon <hjk0472@gmail.com>
2024-04-29 16:25:59 +0900

    ✨Feat: 추가 strike, ball 변수
    for 문 활용 '입력값'과 '정답' 일치 여부에 따라 스트라이크, 볼 추가
    하단 when 문 활용 정답 일치 여부에 따라 문장을 출력하고, 정답이 아닐 경우 지속적으로 숫자를 재입력할 수 있도록 추가 예정

--- src/main/kotlin/lv1/Main.kt
+++ src/main/kotlin/lv1/Main.kt
@@ -7,11 +7,24 @@ fun main() {
     println("Rule2 : 1에서 9 사이 숫자를 입력해야하며 동일한 숫자는 입력할 수 없습니다.")
     print("게임을 시작합니다! 원하시는 숫자 3자리를 입력해주세요 : ")
 
-    val input: Int = readln().toInt()
-    val answerList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val answerNumber = answerList.shuffled()
-
-    println(answerNumber)
+    val oneToNineList = mutableListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val answerList = oneToNineList.shuffled().slice(0..2)
 
 
+    while (true) {
+        val inputList = readln().map{it.digitToInt()}
+
+        var strike = 0
+        var ball = 0
+
+        for (index in 0.. 2) {
+            if (inputList[index] == answerList[index]) {
+                strike += 1
+            }
+            for (i in 0..2) {
+                if(inputList[index] != answerList[index] && inputList[index] == answerList[i])
+                {ball += 1}
+                }
+        }
+    }
 }
