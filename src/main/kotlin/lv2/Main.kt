a48e048f7da1555f494eed4826a833fa661a09e9
hongjinkwon <hjk0472@gmail.com>
2024-05-02 17:20:36 +0900

    ✨Feat: 추가 답안 경우의 수
    추가적인 코드 정리 및 숫자 0도 정답에 포함될 수 있도록 변경 진행
    0이 맨 앞에 올 경우, 예외 문구 출력

--- src/main/kotlin/lv2/Main.kt
+++ src/main/kotlin/lv2/Main.kt
@@ -1,17 +1,17 @@
 package org.example.lv2
 
 fun main() {
-    var ctGame = 0
-    val map: MutableMap<Int, Int> = mutableMapOf()
+    var countGame = 0
+    val recordList: MutableList<Int> = mutableListOf()
 
     while (true) {
-        var ctTry = 0
+        var countTry = 0
 
         print("1.Game Start 2.View Record 3. End The Game : ")
         val selectMenu = readln()
 
-        if (ctGame == 0 && selectMenu == "2") {
-            println("현재까지 확인되는 기록이 없습니다!")
+        if (countGame == 0 && selectMenu == "2") {
+            println("현재까지 확인되는 기록이 없습니다! 게임을 진행해주세요!")
             continue
         }
 
@@ -21,7 +21,9 @@ fun main() {
             }
 
             selectMenu == "2" -> {
-                for (i in 1..map.size) println("$i GAME : ${map[i]} TRY")
+                for (index in 1..recordList.size) {
+                    println("$index GAME : ${recordList[index - 1]} TRY")
+                }
                 continue
             }
 
@@ -31,13 +33,16 @@ fun main() {
             }
         }
 
-        println("Rule1 : 숫자 3자리를 입력해주세요.")
-        println("Rule2 : 1에서 9 사이 숫자를 입력해야하며 동일한 숫자는 입력할 수 없습니다.")
+        println("Rule1 : 3자리 숫자를 입력해주세요.")
+        println("Rule2 : 0에서 9 사이 숫자를 입력해야 하며, 동일한 숫자는 입력할 수 없습니다.")
         print("게임을 시작합니다! 원하시는 숫자 3자리를 입력해주세요 : ")
 
-        val oneToNineList = mutableListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9)
-        val answerList = oneToNineList.shuffled().slice(0..2)
-
+        val readyForAnswerList = mutableListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        val answerList = readyForAnswerList.shuffled().slice(0..2).toMutableList()
+        if (answerList[0] == 0) {
+            answerList[0] = answerList[1]
+            answerList[1] = 0
+        }
 
         while (true) {
             val input = readln()
@@ -45,18 +50,18 @@ fun main() {
             try {
                 val test: Int = input.toInt()
             } catch (e: NumberFormatException) {
-                print("숫자를 입력해주세요! :")
+                print("문자가 아닌 숫자를 입력해주세요! :")
                 continue
             }
 
             val inputList = input.map { it.digitToInt() }
 
             if (input.length != 3) {
-                print("세자리 수를 입력해주세요 : ")
+                print("세자리 수를 입력해주세요! : ")
                 continue
             }
-            if (inputList[0] == 0 || inputList[1] == 0 || inputList[2] == 0) {
-                print("숫자 0은 입력할 수 없습니다 : ")
+            if (inputList[0] == 0) {
+                print("맨 앞자리 숫자에는 0은 입력할 수 없습니다! : ")
                 continue
             }
 
@@ -70,15 +75,16 @@ fun main() {
 
             for (index in 0..2) {
                 if (inputList[index] == answerList[index]) {
-                    strike += 1
+                    strike++
                 }
                 for (i in 0..2) {
                     if (inputList[index] != answerList[index] && inputList[index] == answerList[i]) {
-                        ball += 1
+                        ball++
                     }
                 }
             }
-            ctTry++
+            countTry++
+
             when {
                 strike == 3 -> {
                     println("정답입니다!"); break
@@ -87,11 +93,10 @@ fun main() {
                 strike != 0 && ball != 0 -> println("${strike}스트라이크 ${ball}볼 입니다.")
                 strike != 0 && ball == 0 -> println("${strike}스트라이크입니다.")
                 strike == 0 && ball != 0 -> println("${ball}볼입니다.")
-                else -> println("일치하는 숫자가 없습니다.")
+                else -> println("일치하는 숫자가 없습니다. 호흡 가다듬고.. 천천히..")
             }
         }
-        ctGame++
-        map.put(ctGame, ctTry)
-//        map[ctGame] = ctTry
+        countGame++
+        recordList.add(countTry)
     }
 }
